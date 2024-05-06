cd4c465c0e50a2d696b53107ea5044ece9b94755
MohamedYProg <mohamedelwa7a@gmail.com>
2024-05-06 20:18:19 +0300

    fixed various

--- cart-service/src/main.ts
+++ cart-service/src/main.ts
@@ -3,6 +3,6 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3009);
+  await app.listen(3015);
 }
 bootstrap();
