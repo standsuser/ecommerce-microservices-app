f29cde561aa388ffc703bf9052d028a40bc924ef
standsuser <marioma03@gmail.com>
2024-05-06 22:05:31 +0300

    product runs fine

--- product-service/src/app.module.ts
+++ product-service/src/app.module.ts
@@ -1,22 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { KafkaModule } from './kafka/kafka.module';
-import { TestConsumer } from './kafka/test.consumer';
 import { ProductController } from './product/product.controller';
-import { ProductService } from './product/product.service';
 import { ProductModule } from './product/product.module';
 import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
   imports: [
-    KafkaModule,
     ProductModule,
     MongooseModule.forRoot('mongodb://localhost:27017', {
       connectionName: 'DatabaseConnection',
     }),
   ],
-  controllers: [AppController, ProductController],
-  providers: [AppService, TestConsumer, ProductService],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
--- product-service/src/product/product.module.ts
+++ product-service/src/product/product.module.ts
@@ -1,22 +1,22 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
-import { KafkaModule } from '../kafka/kafka.module'; // Update this path to the actual path of the Kafka module
 import { ProductController } from './product.controller'; 
 import { ProductService } from './product.service'; 
 import { Product, ProductSchema } from './schema/product.schema'; 
 import { Favorite, FavoriteSchema } from './schema/favorite.schema'; 
 import { Category, CategorySchema } from './schema/category.schema'; 
+import { ConsumerService } from 'src/kafka/consumer.service';
+import { ProducerService } from 'src/kafka/producer.service';
 
 @Module({
     imports: [
-        KafkaModule, // Add this line
         MongooseModule.forFeature([
             { name: Product.name, schema: ProductSchema },
             { name: Favorite.name, schema: FavoriteSchema }, 
             { name: Category.name, schema: CategorySchema },
-        ])
+        ]),
     ],
     controllers: [ProductController],
-    providers: [ProductService],
+    providers: [ProductService, ConsumerService, ProducerService],
 })
 export class ProductModule {}
