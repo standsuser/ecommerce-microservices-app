81670cfad28f90c2170cf3fcd0d2585ad15c2e78
MohamedYProg <mohamedelwa7a@gmail.com>
2024-05-09 15:57:28 +0300

    added auth.json

--- payment-processing-service/src/auth/auth.service.ts
+++ payment-processing-service/src/auth/auth.service.ts
@@ -1,22 +1,40 @@
-
-import { Injectable } from '@nestjs/common';
-import axios, { AxiosError, AxiosResponse } from 'axios';
+import { Injectable, OnModuleInit } from '@nestjs/common';
+import axios, { AxiosResponse } from 'axios';
+import * as path from 'path';
+import * as fs from 'fs';
 
 @Injectable()
-export class AuthService {
-    async authenticate(username: string, password: string, apiKey: string): Promise<string> {
-        try {
-          const response: AxiosResponse<any> = await axios.post(
-            'https://accept.paymob.com/api/auth/tokens',
-            { username, password, api_key: apiKey },
-            { headers: { 'Content-Type': 'application/json' } }
-          );
-      
-          // Assuming response includes a token
-          return response?.data?.token;
-        } catch (error) {
-          console.error('Error during authentication:', error);
-          throw new Error('Failed to authenticate');
-        }
-      }
+export class AuthService implements OnModuleInit {
+  private username: string;
+  private password: string;
+  private apiKey: string;
+
+  async onModuleInit() {
+    try {
+      const authData = JSON.parse(fs.readFileSync('./auth/json/auth.json', 'utf8'));
+      this.username = authData.username;
+      this.password = authData.password;
+      this.apiKey = authData.apiKey;
+    } catch (error) {
+      console.error('Error reading auth.json:', error);
+      throw new Error('Failed to read authentication details');
+    }
+  }
+
+  async authenticate(): Promise<string> {
+    try {
+      const response: AxiosResponse<any> = await axios.post(
+        'https://accept.paymob.com/api/auth/tokens',
+        { username: this.username, password: this.password, api_key: this.apiKey },
+        { headers: { 'Content-Type': 'application/json' } }
+      );
+
+      const token = response?.data?.token;
+      console.log('Authentication token:', token);
+      return token;
+    } catch (error) {
+      console.error('Error during authentication:', error);
+      throw new Error('Failed to authenticate');
+    }
+  }
 }
--- payment-processing-service/src/payment/paymob.service.ts
+++ payment-processing-service/src/payment/paymob.service.ts
@@ -6,9 +6,9 @@ import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class PaymobService {
-  constructor(private readonly configService: ConfigService) {}
+  constructor(private readonly configService: ConfigService) { }
   async registerOrder(orderData: any): Promise<number> {
-    const authToken = 'ZXlKaGJHY2lPaUpJVXpVeE1pSXNJblI1Y0NJNklrcFhWQ0o5LmV5SmpiR0Z6Y3lJNklrMWxjbU5vWVc1MElpd2ljSEp2Wm1sc1pWOXdheUk2T1RjME9EWXdMQ0p3YUdGemFDSTZJalZpWlRCaVlqa3lORE0wWVdRMVlUZzRaVEZoWmpReFlUQTNNV1ZtT1RjM1l6QTRNRFF5WlRnME4yUmpaakkxTUdNeE9HRTRNV05qWm1KbVlUQmxPVGdpTENKbGVIQWlPakUzTVRVeU1UVTJOREI5LjlvX0dZcEdIZkhjRm95cGliZDlrc1FKZVVxVVFURXRpall0Wk41QmxxNEk4WmNmTG1DTkpZQnBTTnBKVkRORHdWZXV5SC04ZGF0a01ZeFk4SnF3c2FB';
+    const authToken = 'ZXlKaGJHY2lPaUpJVXpVeE1pSXNJblI1Y0NJNklrcFhWQ0o5LmV5SmpiR0Z6Y3lJNklrMWxjbU5vWVc1MElpd2ljSEp2Wm1sc1pWOXdheUk2T1RjME9EWXdMQ0p3YUdGemFDSTZJalZpWlRCaVlqa3lORE0wWVdRMVlUZzRaVEZoWmpReFlUQTNNV1ZtT1RjM1l6QTRNRFF5WlRnME4yUmpaakkxTUdNeE9HRTRNV05qWm1KbVlUQmxPVGdpTENKbGVIQWlPakUzTVRVeU5UWTJNamg5LjlLd3lnRHEyRDc4QUN5UVd2VVpRb3g4YVJjRE1Fc2ZFWkhXbVNMV3BVTkYtd3pSaTU2bWc3NmpVenEzLWNxQl9HMDFOU1p6dU9UWFViZVpWMDIyVXRR';
 
     try {
       const response: AxiosResponse<any> = await axios.post(
@@ -30,41 +30,41 @@ export class PaymobService {
     }
   }
   async getPaymentKey(paymentData: any): Promise<string> {
-    const authToken = 'ZXlKaGJHY2lPaUpJVXpVeE1pSXNJblI1Y0NJNklrcFhWQ0o5LmV5SmpiR0Z6Y3lJNklrMWxjbU5vWVc1MElpd2ljSEp2Wm1sc1pWOXdheUk2T1RjME9EWXdMQ0p3YUdGemFDSTZJalZpWlRCaVlqa3lORE0wWVdRMVlUZzRaVEZoWmpReFlUQTNNV1ZtT1RjM1l6QTRNRFF5WlRnME4yUmpaakkxTUdNeE9HRTRNV05qWm1KbVlUQmxPVGdpTENKbGVIQWlPakUzTVRVeU1UVTJOREI5LjlvX0dZcEdIZkhjRm95cGliZDlrc1FKZVVxVVFURXRpall0Wk41QmxxNEk4WmNmTG1DTkpZQnBTTnBKVkRORHdWZXV5SC04ZGF0a01ZeFk4SnF3c2FB'; 
+    const authToken = 'ZXlKaGJHY2lPaUpJVXpVeE1pSXNJblI1Y0NJNklrcFhWQ0o5LmV5SmpiR0Z6Y3lJNklrMWxjbU5vWVc1MElpd2ljSEp2Wm1sc1pWOXdheUk2T1RjME9EWXdMQ0p3YUdGemFDSTZJalZpWlRCaVlqa3lORE0wWVdRMVlUZzRaVEZoWmpReFlUQTNNV1ZtT1RjM1l6QTRNRFF5WlRnME4yUmpaakkxTUdNeE9HRTRNV05qWm1KbVlUQmxPVGdpTENKbGVIQWlPakUzTVRVeU5UWTJNamg5LjlLd3lnRHEyRDc4QUN5UVd2VVpRb3g4YVJjRE1Fc2ZFWkhXbVNMV3BVTkYtd3pSaTU2bWc3NmpVenEzLWNxQl9HMDFOU1p6dU9UWFViZVpWMDIyVXRR';
 
     try {
-        const response: AxiosResponse<any> = await axios.post(
-            'https://accept.paymob.com/api/acceptance/payment_keys',
-            paymentData,
-            {
-                headers: {
-                    'Content-Type': 'application/json',
-                    Authorization: `Bearer ${authToken}`,
-                },
-            }
-        );
-
-        // Log the response data for debugging
-        console.log('Paymob API Response:', response.data);
-
-        // Assuming response includes a token
-        return response?.data?.token;
-    } catch (error) {
-        // Log detailed error message and response for debugging
-        console.error('Error obtaining payment key:', error?.message);
-        if (error.response) {
-            console.error('Paymob API Error Response:', error.response.data);
+      const response: AxiosResponse<any> = await axios.post(
+        'https://accept.paymob.com/api/acceptance/payment_keys',
+        paymentData,
+        {
+          headers: {
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${authToken}`,
+          },
         }
-        throw new Error('Failed to obtain payment key');
+      );
+
+      // Log the response data for debugging
+      console.log('Paymob API Response:', response.data);
+
+      // Assuming response includes a token
+      return response?.data?.token;
+    } catch (error) {
+      // Log detailed error message and response for debugging
+      console.error('Error obtaining payment key:', error?.message);
+      if (error.response) {
+        console.error('Paymob API Error Response:', error.response.data);
+      }
+      throw new Error('Failed to obtain payment key');
     }
   }
   async paymentIFrame(paymentToken: string): Promise<void> {
     try {
       const iframeId: string = '844345'; // Your iframe ID
       const url = `https://accept.paymobsolutions.com/api/acceptance/iframes/${iframeId}?payment_token=${paymentToken}`;
-  
+
       const response: AxiosResponse<any> = await axios.post(url);
-  
+
       // Handle response data here if needed
       return response.data;
     } catch (error) {
