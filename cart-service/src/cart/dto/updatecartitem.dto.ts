fdc81d1f53fddb28c26cf20ba152f6158403f8e4
MohamedYProg <mohamedelwa7a@gmail.com>
2024-05-13 15:18:24 +0300

    cart testing

--- cart-service/src/cart/dto/updatecartitem.dto.ts
+++
@@ -1,8 +0,0 @@
-import { IsNotEmpty, IsNumber, Min, IsOptional } from 'class-validator';
-
-export class UpdateCartItemDto {
-    @IsOptional()
-    @IsNumber()
-    @Min(1)
-    quantity?: number;
-}
