fc8b5e5f91e068616e2242fcd53c694a80220239
MohamedYProg <mohamedelwa7a@gmail.com>
2024-05-22 14:00:25 +0300

    fetch cart works frontend

--- cart-service/src/main.ts
+++ cart-service/src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  
+  app.enableCors({
+    origin: 'http://localhost:3000', // Ensure this is your frontend origin
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
+
   await app.listen(3015);
+  console.log('Backend is running on http://localhost:3015');
 }
 bootstrap();
--- frontend-service/pages/api/cartApi.ts
+++ frontend-service/pages/api/cartApi.ts
@@ -11,8 +11,16 @@ export const addItemToCart = async (userId: string, productId: string, addItemDt
   return response.json();
 };
 
-export const getCartItems = async (userId: string) => {
-  const response = await fetch(`http://locahost:3015/cart/items/${userId}`);
+export const getCartItems = async (sessionId: string) => {
+  const response = await fetch(`${API_URL}/cart/guest/${sessionId}/items`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+  if (!response.ok) {
+    throw new Error('Failed to fetch cart items');
+  }
   return response.json();
 };
 
