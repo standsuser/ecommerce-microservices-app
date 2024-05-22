b292ee9be845d8de47731ad233b35dda8b1dc805
standsuser <marioma03@gmail.com>
2024-05-22 20:45:37 +0300

    idk

--- catalog-service/src/catalog/catalog.module.ts
+++ catalog-service/src/catalog/catalog.module.ts
@@ -1,4 +1,3 @@
-/* eslint-disable prettier/prettier */
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { CatalogController } from './catalog.controller';
